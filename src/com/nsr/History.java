5fe3b6b50e0ba1b8fa811575a0883d5c95e76f40
Christoffer Nordeng <panser_bjarne@hotmail.com>
2012-04-26 18:08:52 +0200

    History updates with new metadata.

--- src/com/nsr/History.java
+++ src/com/nsr/History.java
@@ -5,21 +5,46 @@ import java.util.List;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.widget.ListView;
 
 public class History extends Activity {
 	private static final int DIALOG_ERROR = 0;
-	ListView list;
+	private Resources resources;
+	private ListView list;
+	private UpdateReceiver updateReceiver;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.history_view);
-		
+		resources = getResources();
 		list = (ListView)findViewById(R.id.list);
+		updateReceiver = new UpdateReceiver();
+		
+		populate();
+	}
+	
+	@Override
+	protected void onPause() {
+		unregisterReceiver(updateReceiver);
+		super.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		registerReceiver(updateReceiver, new IntentFilter(PlayerService.INTENT_CALLBACK));
+		super.onResume();
+	}
+
+	private void populate() {
 		List<SongData> history = PlayerService.getHistory();
 		if(history==null) {
 			showDialog(DIALOG_ERROR);
@@ -35,9 +60,9 @@ public class History extends Activity {
 		switch(id) {
 		case DIALOG_ERROR :
 			AlertDialog.Builder builder = new AlertDialog.Builder(this);
-			builder.setMessage("No history available.")
+			builder.setMessage(resources.getString(R.string.history_empty))
 				   .setCancelable(false)
-				   .setPositiveButton("Ok", new OnClickListener() {
+				   .setPositiveButton(resources.getString(R.string.generic_ok), new OnClickListener() {
 					@Override
 					public void onClick(DialogInterface dialog, int which) {
 						History.this.finish();
@@ -50,5 +75,13 @@ public class History extends Activity {
 			break;
 		}
 		return dialog;
+	}
+	
+	private class UpdateReceiver extends BroadcastReceiver {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			if(intent.getStringExtra(PlayerService.KEY_MESSAGE).equals(PlayerService.MESSAGE_METADATA_UPDATE))
+				populate();
+		}
 	}
 }
